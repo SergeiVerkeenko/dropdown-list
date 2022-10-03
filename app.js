5b436d870905d81e659251be36431395bc6088de
sergei <sverkeenko1303@gmail.com>
2022-10-03 21:52:23 +0300

    show arrow and validation

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const btn = document.querySelector('.btn');
 const city = document.querySelector('.city');
 
-btn.addEventListener('click', () => {
+function showArrow() {
     if (city.hidden) {
         document.querySelector('.img').style = `
         background-image: url(./assets/Chevron.svg)`
@@ -11,4 +11,13 @@ btn.addEventListener('click', () => {
         document.querySelector('.img').style = `
         background-image: url(./assets/Chevron\ \(1\).svg)`
     }
+}
+
+btn.addEventListener('click', () => {
+    showArrow()
+})
+
+city.addEventListener('click', event => {
+    document.querySelector('.btn p').textContent = event.target.textContent
+    showArrow()
 })
